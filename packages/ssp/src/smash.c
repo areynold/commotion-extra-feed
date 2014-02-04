36d3397d9d10943142801c192184c82edb9133c2
Andrew Reynolds <andrew@opentechinstitute.org>
2014-02-04 16:53:15 -0500

    Printing address of target function for ease of operation

--- packages/ssp/src/smash.c
+++ packages/ssp/src/smash.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+// from http://jvns.ca/blog/2013/10/28/day-17-buffer-overflows/
 // from http://www.thegeekstuff.com/2013/02/stack-smashing-attacks-gcc/
 // http://www.thegeekstuff.com/2013/06/buffer-overflow/
-int main(void)
-{
-    char buff[9];
-    int pass = 0;
+char password[] = "meat";
 
-    printf("Dummy password function to test stack protection\n \
-	to test, run ./smash and enter a string.\n \
-	* string 'commotion' should return 'Match. Root privileges given...'\n \
-	* a string less than 9 characters should return 'No match'\n \
-	* Without stack protection, a string greater than 10 characters should return 'No match. Root privileges given...'\n \
-	* With stack protection, a string greater than 10 characters should return an error and abort\n\n");
-	
+void foo(void) {
+  printf("Stack smashed! You should not see this: %s\n", password);
+  fflush(stdout);
+}
 
-    printf("\n Enter a string: \n");
-    gets(buff);
+int main(int argc, char *argv[]) {
+  char buf[4];
 
-    if(strcmp(buff, "commotion"))
-    {
-        printf ("\n No match \n");
-    }
-    else
-    {
-        printf ("\n Match \n");
-        pass = 1;
-    }
+  printf("Dummy password function to test stack protection\n \
+        to test, run ./smash and enter a string.\n \
+        * string 'commotion' should return 'Match. Root privileges given...'\n \
+        * a string less than 9 characters should return 'No match'\n \
+        * Without stack protection, a string greater than 10 characters should return 'No match. Root privileges given...'\n \
+        * With stack protection, a string greater than 10 characters should return an error and abort\n\n");
 
-    if(pass)
-    {
-       /* Now Give root or admin rights to user*/
-        printf ("Root privileges given to the user \n");
-    }
 
-    return 0;
+  printf("Here is the address of foo: %p\nEnter a string: ", foo);
+  fflush(stdout);
+
+  gets(buf);
+
+  printf("You entered: %s\n", buf);
+  fflush(stdout);
+
+  return 0;
 }
